e037acc1c84ece00fe0b99f5ddf7acf09f60761d
AnnamalaiRajeev <rajeevchinni@gmail.com>
2020-03-02 10:41:15 -0700

    client_server_complete

---
+++ client.py
@@ -0,0 +1,48 @@
+from concurrent import futures
+import grpc
+import test_pb2
+import test_pb2_grpc
+import threading
+import os
+import time
+
+
+def run_client():
+    pid = os.getpid()
+    with grpc.insecure_channel('192.168.56.101:21000') as channel:
+        stub = test_pb2_grpc.FTQueueStub(channel)
+        while True:
+            try:
+                _label_ = input("enter label to create a que")
+                queie_id_response = stub.qCreate(test_pb2.label(value=_label_)).id
+                print(queie_id_response, type(queie_id_response))
+                element = input("enter element to be inserted in que")
+                x = stub.qPush(test_pb2.Push_message(queue_id=queie_id_response,value=element))
+                _id= int(input("enter que id to be popped element"))
+                x = stub.qPop(test_pb2.queueid(id=_id))
+                print("popped element is", x)
+                _id = int(input("enter que id to be topped element"))
+                x = stub.qTop(test_pb2.queueid(id=_id))
+                print("top element is", x)
+                _id = int(input("enter que id to get size"))
+                x = stub.qSize(test_pb2.queueid(id=_id))
+                print("lenght is", x)
+                name = input("enter label to delete queue")
+                x = stub.qDestroy(test_pb2.label(value=name))
+
+            except KeyboardInterrupt:
+                print("Keyboard interrrupt")
+                channel.unsubscribe(close)
+                exit()
+
+
+if __name__ == '__main__':
+    run_client()
+
+
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,112 @@
+from concurrent import futures
+import grpc
+import test_pb2
+import test_pb2_grpc
+import threading
+from collections import deque
+import time
+
+
+class Queue:
+    def __init__(self, number):
+        self.queue = deque()
+        self.id = number
+
+
+class Listener(test_pb2_grpc.FTQueueServicer):
+    queue_map_labels = {}
+    queue_map_id = {}
+    number = 0
+
+    def qCreate(self, request, context):
+        if self.queue_map_labels.get(request.value, False) is not False:
+            return test_pb2.queueid(id=self.queue_map_labels.get(request.value).id) # return Que_id
+        else:
+            new_queue = Queue(number=self.number)
+            self.queue_map_labels[request.value] = new_queue  # map label with new que
+            self.queue_map_id[new_queue.id] = new_queue.queue # map id number to queue
+            id_mapped=self.queue_map_labels.get(request.value).id
+            self.number += 1
+            print('que_id_value', new_queue.id)
+            return test_pb2.queueid(id=id_mapped) # return newly createdQue_id
+
+    def qPush(self, request, context):
+        print('requested_id {} type {}'.format(request.queue_id, type(request.queue_id)))
+        if self.queue_map_id.get(request.queue_id, False) is not False:
+            print("log quueu map", self.queue_map_id.get(request.queue_id, False))
+            queue_to_push_context = self.queue_map_id.get(request.queue_id)
+            queue_to_push_context.append(request.value)
+            print("added value {} to queue {}".format(request.value, request.queue_id))
+            return test_pb2.void()
+        else:
+            return test_pb2.void()
+
+    def qId(self, request, context):
+        label_requested = request.label
+        if self.queue_map_labels.get(label_requested, False) is not False:
+            return test_pb2.queueid(id=self.queue_map_labels.get(label_requested, False).id)
+        else:
+            print("Que name doesnt exist")
+            return test_pb2.queueid(id=-1) #return -1 que if label not present
+
+    def qPop(self, request, context):
+        que_to_pop_from = request.id
+        if self.queue_map_id.get(que_to_pop_from, False) is not False:
+            if len(self.queue_map_id[que_to_pop_from]) > 0:
+                element = self.queue_map_id[que_to_pop_from].pop() #returns an item from the front of a queue
+            else:
+                element = ' '
+            return test_pb2.Item(value=element)
+        else:
+            return test_pb2.Item(value=' ')
+
+    def qTop(self, request, context):
+        que_to_pop_from = request.id
+        if self.queue_map_id.get(que_to_pop_from, False) is not False:
+            if len(self.queue_map_id[que_to_pop_from]) > 0:
+                element = self.queue_map_id[que_to_pop_from][0]  # returns an item from the front of a queue
+            else:
+                element=' '
+            return test_pb2.Item(value=element)
+        else:
+            return test_pb2.Item(value=' ')
+
+    def qSize(self, request, context):
+        que_to_pop_from = request.id
+        if self.queue_map_id.get(que_to_pop_from, False) is not False:
+            element = len(self.queue_map_id[que_to_pop_from])  # returns an item from the front of a queue
+            return test_pb2.QueLength(length=element)
+        else:
+            return test_pb2.QueLength(length=-1)
+
+    def qDestroy(self, request, context):
+        que_label = request.value
+        if self.queue_map_labels.get(que_label, False) is not False:
+            del self.queue_map_id[self.queue_map_labels[que_label].id]
+            del self.queue_map_labels[que_label]
+            return test_pb2.void()
+
+        else:
+            return test_pb2.void()
+
+
+def serve_ftqueue_service():
+    print("Starting FTqueue service on port 21000")
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    try:
+        test_pb2_grpc.add_FTQueueServicer_to_server(Listener(),server)
+        server.add_insecure_port("0.0.0.0:21000")
+        server.start()
+        while True:
+            time.sleep(5)
+            obj_2 = Listener()
+            print(obj_2.queue_map_id)
+            print(obj_2.queue_map_labels)
+            pass
+    except KeyboardInterrupt:
+        print("Gracefull exit")
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve_ftqueue_service()
